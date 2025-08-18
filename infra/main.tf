8f72c4e64f6df978d896f720537784040715940f
Pattabiraman <pattabimanu@gmail.com>
2025-08-18 18:12:32 +0530

    terraform update

--- infra/main.tf
+++ infra/main.tf
@@ -14,4 +14,39 @@ terraform {
         dynamodb_table = var.backend_dynamodb_table
         encrypt       = true
     }
-}   
+}
+
+module "vpc"{
+    source = "./modules/vpc"
+    vpc_cidr = var.vpc_cidr
+    public_subnet_cidr = var.public_subnet_cidr
+    private_subnet_cidr = var.private_subnet_cidr
+    availability_zone = var.availability_zone
+    project = var.project
+    aws_region = var.aws_region
+
+}
+
+module "eks" {
+  source  = "./modules/eks"
+  cluster_name = var.cluster_name
+  cluster_version = var.cluster_version
+  node_instance_type = var.node_instance_type
+  desired_capacity = var.desired_capacity
+  max_capacity = var.max_capacity
+  min_capacity = var.min_capacity
+
+  #passing VPC and subnet information from the VPC module
+  vpc_id = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnet_ids
+  }
+
+module "jenkins" {
+  source = "modules/jenkins"
+  aws_region = var.aws_region
+  
+  #passing VPC and subnet information from the VPC module
+  vpc_id = module.vpc.vpc_id
+  public_subnet_ids = module.vpc.public_subnet_ids
+  private_subnet_ids = module.vpc.private_subnet_ids
+}
